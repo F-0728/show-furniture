d9e78579467cccee2284ff12ca8c8958f999f4ba
F-0728 <fugue2767@icloud.com>
2024-03-10 21:37:45 +0900

    add: implemented table

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,6 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import React from 'react';
 import { TableContainer, Table, Thead, Tbody, Tr, Td, Th } from '@chakra-ui/react';
-import { ColumnDef, getCoreRowModel, useReactTable, flexRender } from "@tanstack/react-table";
 
 // APIを叩くよ！！
 const isError = (error: unknown): error is Error => {
@@ -12,25 +11,12 @@ const isError = (error: unknown): error is Error => {
 type Furniture = {
     place: string;
     characters: string[];
-    lack: any;
+    //lack: any;
 };
 
-const columns: ColumnDef<Furniture, any>[] = [
-    {
-        accessorKey: 'place',
-        header: '設置場所',
-    },
-    {
-        accessorKey: 'characters',
-        header: 'キャラクター',
-    },
-    {
-        accessorKey: 'lack',
-        header: '不足',
-    },
-];
+const columns: string[] = ["place", "characters"];
 
-const BasicTable: React.FC = () => {
+const BasicTable = () => {
     const [furnitureData, setFurnitureData] = useState<(Furniture | null)[]>([]);
 
     const fetchFurniture = async () => {
@@ -48,48 +34,24 @@ const BasicTable: React.FC = () => {
         fetchFurniture();
     }, []);
 
-    const table = useReactTable({
-        columns,
-        data: furnitureData,
-        getCoreRowModel: getCoreRowModel(),
-    });
-
     return (
-        <TableContainer>
-            <Table>
-                <Thead>
-                    {table.getHeaderGroups().map((headerGroup) => (
-                        <Tr key={headerGroup.id}>
-                            {headerGroup.headers.map((header) => {
-                                return (
-                                    <Th
-                                        key={header.id}
-                                    >
-                                        {flexRender(
-                                            header.column.columnDef.header,
-                                            header.getContext()
-                                        )}
-                                    </Th>
-                                );
-                            })}
-                        </Tr>
+        <Table variant="simple">
+            <Thead>
+                <Tr>
+                    {columns.map((column) => (
+                        <Th key={column}>{column}</Th>
                     ))}
-                </Thead>
-                <Tbody>
-                    {table.getRowModel().rows.map((row) => (
-                        <Tr key={row.id}>
-                            {row.getVisibleCells().map((cell) => {
-                                return (
-                                    <Td key={cell.id}>
-                                        {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                                    </Td>
-                                );
-                            })}
-                        </Tr>
-                    ))}
-                </Tbody>
-            </Table>
-        </TableContainer>
+                </Tr>
+            </Thead>
+            <Tbody>
+                {furnitureData.map((furniture) => (
+                    <Tr>
+                        <Td>{furniture.place}</Td>
+                        <Td>{furniture.characters.join(", ")}</Td>
+                    </Tr>
+                ))}
+            </Tbody>
+        </Table>
     );
 };
 
